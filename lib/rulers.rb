3d88683b763fc128838039e5103ab2d29509220d
Jonathan Simpson <jsimpson.github@gmail.com>
2020-10-30 16:10:11 -0700

    routing

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,4 +1,5 @@
-require "rulers/array"
+require "rulers/routing"
+
 require "rulers/version"
 
 module Rulers
@@ -6,11 +7,18 @@ module Rulers
 
   class Application
     def call(env)
-      [
-        200,
-        { 'Content-Type' => 'text/html' },
-        ["Hello, World!"]
-      ]
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, { 'Content-Type' => 'text/html' }, [text]]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
 end
